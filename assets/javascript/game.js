73815242829bb5236bc1607ccdfa905c94ff4335
unknown <hannahuseva@gmail.com>
2019-04-27 23:29:01 -0500

    Still nonfunctional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,4 @@
-//* The random number shown at the start of the game should be between 19 - 120.
-//* Each crystal should have a random hidden value between 1 - 12.
-
 $(document).ready(function() {
-
 
 var wins=0
 var losses=0
@@ -15,32 +11,41 @@ var rubyvalue = Math.floor(Math.random() * ((12-1)+1)+1)
 
 console.log (diamondvalue, emeraldvalue, opalvalue, rubyvalue)
 
+
 $("#wins").append(wins)
 $("#losses").append(losses)
 $("#randomnumber").append(randomNumber)
 $("#totscore").append(totscore)
 
-$("#diamond").on("click", function em () {
+
+//I did not fugure out the way to get variable totscore from under the click event function
+
+$("#diamond").on("click",function (){
     totscore=totscore+diamondvalue
-   console.log (totscore)
+    return totscore
 })
 
 $("#emerald").on("click", function() {
     totscore=totscore+emeraldvalue
+    return totscore
 })
 
 $("#opal").on("click", function() {
     totscore=totscore+opalvalue
+    return totscore
 })
 
 $("#ruby").on("click", function() {
     totscore=totscore+rubyvalue
+    return totscore
 })
 
-console.log (totscore)
+if (totscore == randomNumber) {
+    wins++
+}
 
-
-
-
+if (totscore < randomNumber) {
+    losses++
+}
 
 })
